328cea81976cd7c239521224a5e8c3e6194fe4b5
fireb1rdz <gustavonelsonpaganelli@gmail.com>
2026-01-21 20:12:00 -0300

    entity and user listing, creating and editing functions created

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,10 @@
-from urllib.parse import urlparse
-from django.urls import path, include
-from .views import index
+from django.urls import path
+from .views import UserCreateView, UserListView, UserUpdateView, UserDeleteView
 
+app_name = 'users'
 urlpatterns = [
-    path('', index, name='index'),
+    path('usuarios/criar/', UserCreateView.as_view(), name='user_create'),
+    path('usuarios/listar/', UserListView.as_view(), name='user_list'),
+    path('usuarios/editar/<int:pk>/', UserUpdateView.as_view(), name='user_update'),
+    path('usuarios/excluir/<int:pk>/', UserDeleteView.as_view(), name='user_delete'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib import messages
+from apps.users.models import User
+from apps.users.forms import UserForm
 
 
-def index(request):
-    return render(request, 'users/index.html')
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserForm
+    success_url = "/usuarios/listar/"
+    def form_valid(self, form):
+        messages.success(self.request, 'Usuário criado com sucesso!')
+        return super().form_valid(form)
+    
+class UserListView(ListView):
+    model = User
+    context_object_name = "users"
+    paginate_by = 10
+    
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = UserForm
+    success_url = "/usuarios/listar/"
+    def form_valid(self, form):
+        messages.success(self.request, 'Usuário atualizado com sucesso!')
+        return super().form_valid(form)    
+        
+class UserDeleteView(DeleteView):
+    model = User
+    success_url = "/usuarios/listar/"
